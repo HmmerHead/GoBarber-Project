b7f03df4b9d20ec0d67561e5603fcb7edb91eb83
HmmerHead <soad_guilherme@hotmail.com>
2020-03-22 20:29:15 -0300

    modificaoes no index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const soma = (a, b) => a + b;
+import React from 'react';
+import { ReactDOM } from 'react-dom';
 
-alert (soma(1, 25));
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('app'));
